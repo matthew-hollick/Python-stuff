26cf5ae1f008d292221f05097442724fef8ef1f6
Matthew Hollick <22219356+matthew-hollick@users.noreply.github.com>
2024-12-14 00:10:47 +0000

    formatting

--- prometheus-to-logstash/fpm.sh
+++ prometheus-to-logstash/fpm.sh
@@ -30,4 +30,3 @@ fpm \
   --deb-default prometheus-to-logstash.default \
   --description "Pull data from a prometheus exporter and forward it to logstash" \
   prometheus-to-logstash.py=/usr/local/bin/prometheus-to-logstash.py logstash-file-output.conf=/etc/logstash/conf.d/logstash-file-output.conf logstash-http-input.conf=/etc/logstash/conf.d/logstash-http-input.conf
-
